ea2cb5298ebe2fdb2e8a5258b99c054929290338
wanglifeng717 <2498202385@qq.com>
2017-12-15 16:11:12 +0800

    使用JDBC调用存储过程或存储函数
    1.按照5步走的方式。

--- src/com/tongji/DBUtils/JDBCTools.java
+++ src/com/tongji/DBUtils/JDBCTools.java
@@ -1,162 +1,154 @@
+
 package com.tongji.DBUtils;
+
 
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/**
+ * 
+* Title: JDBCTools 
+* Description: 数据库工具类
+* 包含了数据库连接，数据连接关闭，数据库事务开启，回滚，关闭的函数。 
+* @author mdm(computer in lab)  
+* @date 2017年12月14日
+ */
 public class JDBCTools {
-	
-	
+
 	/**
-	 * 执行 SQL 的方法,使用preparedstatement构成重载
+	 * 功能：提交数据库事务
 	 * 
-	 * @param sql: insert, update 或 delete。 而不包含 select
+	 * @param connection
 	 */
-	public static void update(String sql,Object ... args )
-	{//用可变参数更好，不然程序里面还要封装成数组才能用。这样写的话，你爱传几个就传几个
-		Connection connection=null;
-		PreparedStatement preparedStatement=null;
-		try {
-			connection=JDBCTools.getConnection();
-			preparedStatement=connection.prepareStatement(sql);
-			
-			for(int i=0;i<args.length;i++)
-			{
-				preparedStatement.setObject(i+1, args[i]);
-			}
-			
-			preparedStatement.executeUpdate();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			JDBCTools.release(preparedStatement, connection);
-		}
-	}
-	
-	/**
-	 * 执行 SQL 的方法
-	 * 
-	 * @param sql: insert, update 或 delete。 而不包含 select
-	 */
-	public static void update(String sql) {
-		Connection connection = null;
-		Statement statement = null;
-
-		try {
-			// 1. 获取数据库连接
-			connection = getConnection();
-
-			// 2. 调用 Connection 对象的 createStatement() 方法获取 Statement 对象
-			statement = connection.createStatement();
-
-			// 4. 发送 SQL 语句: 调用 Statement 对象的 executeUpdate(sql) 方法
-			statement.executeUpdate(sql);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// 5. 关闭数据库资源: 由里向外关闭.
-			release(null,statement, connection);
-		}
-	}
-	
-	
-	
-	/*更加高级的版本：关闭Statement,connection,resultSet*/
-	public static void release(ResultSet resultSet,Statement statement,Connection connection)
+	public static void commit(Connection connection)
 	{
-		if(connection!=null)
-		 {
-			 try {
-				connection.close();
+		if(connection != null)
+		{
+			try {
+				connection.commit();
 			} catch (Exception e) {
+				// TODO: handle exception
 				e.printStackTrace();
 			}
-		 }
-		
-		if(resultSet!=null)
-		 {
-			 try {
-				resultSet.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		 }
-		 
-		 if(statement!=null)
-		 {
-			 try {
-				statement.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		 }
+		}
 	}
-
 	/**
-	 * 关闭 Statement 和 Connection
-	 * @param statement
-	 * @param conn
+	 * 功能：回滚数据库事务
+	 * 
+	 * @param connection
 	 */
-	 public static void release(Statement statement,Connection connection)
-	 {
-		 if(statement!=null)
-		 {
-			 try {
-				statement.close();
+	public static void rollback(Connection connection) {
+		if(connection!=null) {
+			try {
+				connection.rollback();
 			} catch (Exception e) {
+				// TODO: handle exception
 				e.printStackTrace();
 			}
-		 }
-		 
-		 if(connection!=null)
-		 {
-			 try {
-				connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		 }
-					 
-	 }
-	 
-	
-	
+		}
+	}
 	/**
-	 * 1. 获取连接的方法. 通过读取配置文件从数据库服务器获取一个连接.
+	 * 功能：开启数据库事务
+	 * 
+	 * @param connection
+	 */
+	public static void beginTx(Connection connection) {
+		if(connection!=null) {
+			try {
+				connection.setAutoCommit(false);
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static DataSource dataSource = null;
+	//数据库连接池应只被初始化一次. 
+		static{
+			dataSource = new ComboPooledDataSource("helloc3p0");
+		}
+		/**
+		 * 功能：以后所有的获取连接的方式都从数据库连接池中获取即可
+		 * 
+		 * @return
+		 * @throws Exception
+		 */
+		public static Connection getConnection() throws Exception {
+			return dataSource.getConnection();
+		}
+	/**
+	 * 功能：获取数据库连接
 	 * 
 	 * @return
 	 * @throws Exception
 	 */
-	public static Connection getConnection() throws Exception {
-		// 1. 准备连接数据库的 4 个字符串.
-		// 1). 创建 Properties 对象
+	public static Connection getConnection2() throws Exception {
 		Properties properties = new Properties();
+		InputStream inStream = JDBCTools.class.getClassLoader()
+				.getResourceAsStream("jdbc.properties");
+		properties.load(inStream);
 
-		// 2). 获取 jdbc.properties 对应的输入流
-		InputStream in = 
-				JDBCTools.class.getClassLoader().getResourceAsStream(
-				"jdbc.properties");
-
-		// 3). 加载 2） 对应的输入流
-		properties.load(in);
-
-		// 4). 具体决定 user, password 等4 个字符串.
+		// 1. 准备获取连接的 4 个字符串: user, password, jdbcUrl, driverClass
 		String user = properties.getProperty("user");
 		String password = properties.getProperty("password");
 		String jdbcUrl = properties.getProperty("jdbcUrl");
-		String driver = properties.getProperty("driver");
+		String driverClass = properties.getProperty("driver");
+		// 2. 加载驱动: Class.forName(driverClass)
+		Class.forName(driverClass);
 
-		// 2. 加载数据库驱动程序(对应的 Driver 实现类中有注册驱动的静态代码块.)
-		Class.forName(driver);
-
-		// 3. 通过 DriverManager 的 getConnection() 方法获取数据库连接.
-		return DriverManager.getConnection(jdbcUrl, user, password);
+		// 3. 调用
+		// DriverManager.getConnection(jdbcUrl, user, password)
+		// 获取数据库连接
+		Connection connection = DriverManager.getConnection(jdbcUrl, user,
+				password);
+		return connection;
 	}
+
+	/**
+	 * 功能：释放数据库连接
+	 * 
+	 * @param resultSet
+	 * @param statement
+	 * @param connection
+	 */
+	public static void releaseDB(ResultSet resultSet, Statement statement,
+			Connection connection) {
+
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (connection != null) {
+			try {
+				//数据库连接池的 Connection 对象进行 close 时
+				//并不是真的进行关闭, 而是把该数据库连接会归还到数据库连接池中. 
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
